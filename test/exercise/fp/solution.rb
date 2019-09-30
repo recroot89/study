8f10f6a4fc34b7bd95874ef3f5b39c711ac8b87d
recroot89 <recroot89@gmail.com>
2019-09-30 14:16:15 +0500

    fix solution

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,30 +4,18 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        ratings = _array.map(&:to_hash).reduce(total: 0, count: 0) do |acc, film|
-          if film_has_country_and_rating?(film) && film['country'].split(',').count > 1
-            {
-              count: acc[:count] + 1,
-              total: acc[:total] + film['rating_kinopoisk'].to_f
-            }
-          else
-            acc
-          end
+      def rating(array)
+        filtered_films = array.select do |film|
+          !film['country'].nil? &&
+            film['country'].split(',').count > 1 &&
+            film['rating_kinopoisk'].to_f.positive?
         end
-        ratings[:total] / ratings[:count]
+        filtered_films.inject(0.0) { |sum, film| sum + film['rating_kinopoisk'].to_f } / filtered_films.count
       end
 
-      def chars_count(_films, _threshold)
-        _films.map(&:to_hash).reduce(0) do |acc, film|
-          film['rating_kinopoisk'].to_f >= _threshold ? acc + film['name'].count('и') : acc
-        end
-      end
-
-      private
-
-      def film_has_country_and_rating?(film)
-        !(film['country'].nil? || film['rating_kinopoisk'].to_f.zero?)
+      def chars_count(films, threshold)
+        films.select { |film| film['rating_kinopoisk'].to_f >= threshold }
+             .inject(0) { |sum, film| sum + film['name'].count('и') }
       end
     end
   end
